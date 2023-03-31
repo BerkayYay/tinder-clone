d6b507cfd9edb484c7a228d214547eb36eef0ea7
Berkay YAY <berkayyay7@gmail.com>
2023-03-31 18:19:09 +0300

    React native and tailwind setup done.

---
+++ TinderClone/App.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View className="flex items-center justify-center h-full bg-gray-100">
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
