03ee69bc4fd8e300c5f642a623ac457b35332324
Berkay YAY <berkayyay7@gmail.com>
2023-04-04 12:15:15 +0300

    Login screen designed.

--- TinderClone/screens/LoginScreen.js
+++ TinderClone/screens/LoginScreen.js
@@ -1,13 +1,41 @@
-import {View, Text, Button} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  Button,
+  ImageBackground,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useLayoutEffect} from 'react';
 import useAuth from '../hooks/useAuth';
+import {useNavigation} from '@react-navigation/native';
 
 const LoginScreen = () => {
   const {singInWithGoogleAsync, loading} = useAuth();
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>{loading ? 'Loading...' : 'I am the LoginScreen'}</Text>
-      <Button title="Login" onPress={() => singInWithGoogleAsync()} />
+    <View className="flex-1">
+      <ImageBackground
+        resizeMode="cover"
+        className="flex-1"
+        source={{
+          uri: 'https://tinder.com/static/tinder.png',
+        }}>
+        <TouchableOpacity
+          style={{marginHorizontal: '25%'}}
+          className="absolute bottom-40 w-52 bg-white p-4 rounded-2xl"
+          onPress={singInWithGoogleAsync}>
+          <Text className="text-center font-semibold">
+            Sign in & get swiping
+          </Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 };
